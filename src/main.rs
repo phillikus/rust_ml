2a9f0eebbe4a62bbd08401275376008a8874d888
Philipp <pengelmann92@googlemail.com>
2018-12-11 21:27:07 +0100

    Implement simple linear regression + mean squared error, check out plotting libraries

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
+extern crate dataplotlib;
 extern crate linear_regression;
 
+use dataplotlib::util::{linspace, zip2};
+use dataplotlib::plotbuilder::PlotBuilder2D;
+use dataplotlib::plotter::Plotter;
 
 pub fn main() {
+    let mut model = linear_regression::LinearRegression::new();
+    let x_values = vec![1f32, 2f32, 3f32, 4f32, 5f32];
+    let y_values = vec![1f32, 3f32, 2f32, 3f32, 5f32];
+
+    model.fit(&x_values, &y_values);
+
+    let accuracy = model.evaluate(&x_values, &y_values);
+    println!("Accuracy: {0}", accuracy);
+
+    let mut pb = PlotBuilder2D::new();
+    let x_values_f64 = x_values.into_iter().map(|x| x as f64).collect();
+    let y_values_f64 = y_values.into_iter().map(|x| x as f64).collect();
+
+    pb.add_color_xy(zip2(&x_values_f64, &y_values_f64), [1.0, 0.0, 0.0, 1.0]);
+    
+    let mut plt = Plotter::new();
+    plt.plot2d(pb);
+    plt.join();
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-fn mean(values : &Vec<f32>) -> f32 {
+pub fn mean(values : &Vec<f32>) -> f32 {
     if values.len() == 0 {
         return 0f32;
     }
@@ -6,7 +6,7 @@ fn mean(values : &Vec<f32>) -> f32 {
     return values.iter().sum::<f32>() / (values.len() as f32);
 }
 
-fn variance(values : &Vec<f32>) -> f32 {
+pub fn variance(values : &Vec<f32>) -> f32 {
     if values.len() == 0 {
         return 0f32;
     }
@@ -15,7 +15,7 @@ fn variance(values : &Vec<f32>) -> f32 {
     return values.iter().map(|x| f32::powf(x - mean, 2 as f32)).sum::<f32>() / values.len() as f32;
 }
 
-fn covariance(x_values : &Vec<f32>, y_values : &Vec<f32>) -> f32 {
+pub fn covariance(x_values : &Vec<f32>, y_values : &Vec<f32>) -> f32 {
     if x_values.len() != y_values.len() {
         panic!("x_values and y_values must be of equal length.");
     }
@@ -30,7 +30,7 @@ fn covariance(x_values : &Vec<f32>, y_values : &Vec<f32>) -> f32 {
     let mean_x = mean(x_values);
     let mean_y = mean(y_values);
 
-    for i in 0..length-1 {
+    for i in 0..length {
         covariance += (x_values[i] - mean_x) * (y_values[i] - mean_y)
     }
 
@@ -73,16 +73,16 @@ mod tests {
     }
 
     #[test]
-    fn covariance_should_be_greater_than_0_value_for_linear_vecs() {
-        let mut x_values = vec![1f32, 2f32, 3f32, 4f32, 5f32];
-        let mut y_values = vec![5f32, 10f32, 15f32, 20f32, 25f32];
-        assert_eq!(6f32, covariance(&x_values, &y_values));
+    fn covariance_should_be_positive_for_linear_vecs() {
+        let x_values = vec![1f32, 2f32, 3f32, 4f32, 5f32];
+        let y_values = vec![1f32, 3f32, 2f32, 3f32, 5f32];
+        assert_eq!(1.6f32, covariance(&x_values, &y_values));
     }
 
     #[test]
-    fn covariance_should_be_smaller_than_1_for_unrelated_vecs() {
-        let mut x_values = vec![1f32, 2f32, 3f32, 4f32, 5f32];
-        let mut y_values = vec![0.5f32, 4f32, 1f32, -5f32, 32f32];
-        assert_eq!(0.6f32, covariance(&x_values, &y_values));
+    fn covariance_should_be_negative_for_inverse_vecs() {
+        let x_values = vec![1f32, 2f32, 3f32, 4f32, 5f32];
+        let y_values = vec![0.5f32, 4f32, 1f32, -5f32, 4f32];
+        assert_eq!(-0.4f32, covariance(&x_values, &y_values));
     }
 }
